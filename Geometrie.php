478139498e1a29d55f8dac325b60a737966d93d4
Schaukelente <schaukelente@gmx.de>
2020-01-10 11:41:16 +0100

    geometrie.php geaendert
    Rechteckberechnung eingefuegt

--- Geometrie.php
+++ Geometrie.php
@@ -17,23 +17,42 @@
 		<br><br>
 		
 		<?php
-			$_ErgebnisUmf = "";
-			$_ErgebnisFl = "";
-			if(isset($_POST["berechnen"]))
+			$_ErgebnisUmfK = "";
+			$_ErgebnisFlK = "";
+			$_ErgebnisUmfR = "";
+			$_ErgebnisFlR = "";			
+			if(isset($_POST["berechnenKreis"]))
 			{
-				$_ErgebnisUmf = number_format(2*$_POST["radius"]*pi(),2)." cm";
-				$_ErgebnisFl = number_format($_POST["radius"]*$_POST["radius"]*pi(),2). " cm²";
+				$_ErgebnisUmfK = number_format(2*$_POST["radius"]*pi(),2)." cm";
+				$_ErgebnisFlK = number_format($_POST["radius"]*$_POST["radius"]*pi(),2). " cm²";
 			}
+			if(isset($_POST["berechnenRechteck"]))
+			{
+				$_ErgebnisUmfR = number_format(2*$_POST["l1"]+2*$_POST["l2"],2)." cm";
+				$_ErgebnisFlR = number_format($_POST["l1"]*$_POST["l2"],2). " cm²";
+			}			
 		?>
 			
 		<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
 		
-			Radius in cm: &nbsp <input type="text" id="radius" style="background-color:#E4EDDF" value="" name="radius" autocomplete="off" required>
-			<input type="submit" name="berechnen" value="Berechnen">
+			Radius in cm: &nbsp <input type="text" id="radius" style="background-color:#E4EDDF" value="" name="radius" autocomplete="off">
+			<input type="submit" name="berechnenKreis" value="Berechnen">
 			<br><br><br>
-			Umfang: &nbsp <input type="text" id="umfang" size=10 style="background-color:#E4EDDF" value="<?php echo $_ErgebnisUmf ?>"  name="umfang" >
+			Kreisumfang: &nbsp <input type="text" id="umfang" size=10 style="background-color:#E4EDDF" value="<?php echo $_ErgebnisUmfK ?>"  name="umfangK" >
 			<br><br>
-			Fläche: &nbsp &nbsp <input type="text" id="flaeche" size=10 style="background-color:#E4EDDF" value="<?php echo $_ErgebnisFl ?>"  name="flaeche" >
+			Kreisfläche: &nbsp &nbsp <input type="text" id="flaeche" size=10 style="background-color:#E4EDDF" value="<?php echo $_ErgebnisFlK ?>"  name="flaecheK" >
+			<br><br>	
+			
+		<p class='grossblau'> Rechteckberechnung:</p>
+		<br><br>			
+			Kantenlänge1 in cm: &nbsp <input type="text" id="radius" style="background-color:#E4EDDF" value="" name="l1" autocomplete="off">
+			<br><br>
+			Kantenlänge2 in cm: &nbsp <input type="text" id="radius" style="background-color:#E4EDDF" value="" name="l2" autocomplete="off">
+			<input type="submit" name="berechnenRechteck" value="Berechnen">
+			<br><br>			
+			Umfang des Rechtecks: &nbsp <input type="text" id="umfang" size=10 style="background-color:#E4EDDF" value="<?php echo $_ErgebnisUmfR ?>"  name="umfangR" >
+			<br><br>
+			Fläche des Rechtecks: &nbsp &nbsp <input type="text" id="flaeche" size=10 style="background-color:#E4EDDF" value="<?php echo $_ErgebnisFlR ?>"  name="flaecheR" >			
 			</form>
 	</body>
 </html>
